a3354fa4fb9fb8250e6cbbd0f2baaa3b1614ab5c
Barthe <barthepacome@gmail.com>
2023-07-27 10:17:03 +0200

    Index & Fighter - 1

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const Fighter = require('./src/Fighter.js');
+
+const Fighters = [
+    Heracles = new Fighter("🧔 Heracles",20,6),
+    NemeanLion = new Fighter("🦁 Nemean Lion",11,13),
+];
+
+let Round = 0
+
+while (Fighters[0].life > 0 && Fighters[1].life > 0) {
+
+    // Fight
+
+    Heracles.Fight(NemeanLion);
+    NemeanLion.Fight(Heracles);
+
+    // Round Info
+
+    Round += 1
+
+    console.log("⏱️ Round: #" + Round)
+
+    console.log(Fighters[0].name + ": ❤️" + Fighters[0].life)
+    console.log(Fighters[1].name + ": ❤️" + Fighters[1].life)
+
+};
+
+if (Fighters[1].IsAlive == true) {
+
+    console.log("⏱️ Round: #Result")
+    console.log(Fighters[0].name + " was defeated by " + Fighters[1].name)
+    
+
+} else {
+
+    console.log("⏱️ Round: #Result")
+    console.log(Fighters[0].name + " was defeated by " + Fighters[1].name)
+
+};
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,54 @@
 /* Fighter class definition */
+
+const MAX_LIFE = 100;
+
+module.exports = class Figther {
+
+    constructor (name,strength,dexterity) {
+
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE;
+    
+    };
+    
+    // Fight
+    // Attack the target selected
+
+    Fight (Target) {
+
+        const Attack = Math.floor((Math.random() * this.strength) + 1);
+
+        if (Attack > Target.dexterity) {
+
+            const Damage = Attack - Target.dexterity;
+
+            if (Damage >= Target.life) {
+
+                return Target.life = 0;
+
+            } else {
+
+                return Target.life -= Damage;
+
+            };
+
+        } else {
+
+            return Target.life;
+
+        };
+
+    };
+
+    // IsAlive
+    // return true if alive or false if dead
+
+    IsAlive () {
+
+        return this.life > 0;
+
+    };
+
+};
